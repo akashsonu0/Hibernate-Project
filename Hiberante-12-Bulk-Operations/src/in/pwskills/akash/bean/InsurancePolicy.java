f51a5e9829a0650c46a14761b706c735865e15f7
akashsonu0 <akashsonu7744@gmail.com>
2026-09-07 17:14:59 +0530

    Bulk-Operation

--- Hiberante-12-Bulk-Operations/src/in/pwskills/akash/bean/InsurancePolicy.java
+++ Hiberante-12-Bulk-Operations/src/in/pwskills/akash/bean/InsurancePolicy.java
@@ -8,10 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Entity
 public class InsurancePolicy implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
