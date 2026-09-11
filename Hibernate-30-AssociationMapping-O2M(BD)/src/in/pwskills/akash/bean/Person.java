d1341c3420dcadc8aaa327c7302e2b8716893ca1
akashsonu0 <akashsonu7744@gmail.com>
2026-09-11 14:44:05 +0530

    O2M(Bi-Direction)

--- Hibernate-30-AssociationMapping-O2M(BD)/src/in/pwskills/akash/bean/Person.java
+++ Hibernate-30-AssociationMapping-O2M(BD)/src/in/pwskills/akash/bean/Person.java
@@ -2,14 +2,12 @@ package in.pwskills.akash.bean;
 
 import java.io.Serializable;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
